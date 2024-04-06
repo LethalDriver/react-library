4c408a0474e25ac9239d44f592eb9bd0bf22ff1c
mwdziak <mwdziak@gmail.com>
2024-04-06 22:03:11 +0200

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
+
+import App from './App'; // Import the App component
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) { 
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>,
+  );
+}
