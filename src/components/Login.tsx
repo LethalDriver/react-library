0ca7887f8883189f867b4a21d5819f37f870f25f
mwdziak <mwdziak@gmail.com>
2024-04-08 14:59:14 +0200

    Refactor login component and add Formik form validation

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,12 @@
-
+import * as Yup from "yup";
+import {
+  Formik,
+  Field,
+  Form,
+  ErrorMessage,
+  FieldInputProps,
+  FormikProps,
+} from "formik";
 
 import {
   Flex,
@@ -12,53 +20,101 @@ import {
   Heading,
   Text,
   useColorModeValue,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required("Required"),
+  password: Yup.string().required("Required"),
+});
 
 export default function SimpleCard() {
   return (
-    <Flex
-    minH={'calc(100vh - 18rem)'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}>
-                <Checkbox>Remember me</Checkbox>
-                <Text color={'blue.400'}>Forgot password?</Text>
-              </Stack>
-              <Button
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}>
-                Sign in
-              </Button>
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Flex
+          minH={"calc(100vh - 18rem)"}
+          align={"center"}
+          justify={"center"}
+          bg={useColorModeValue("gray.50", "gray.800")}
+        >
+          <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+            <Stack align={"center"}>
+              <Heading fontSize={"4xl"}>Sign in to your account</Heading>
             </Stack>
+            <Box
+              rounded={"lg"}
+              bg={useColorModeValue("white", "gray.700")}
+              boxShadow={"lg"}
+              p={8}
+            >
+              <Form>
+                <Stack spacing={4}>
+                  <Field name="email">
+                    {({
+                      field,
+                      form,
+                    }: {
+                      field: FieldInputProps<any>;
+                      form: FormikProps<any>;
+                    }) => (
+                      <FormControl id="email">
+                        <FormLabel>Email address</FormLabel>
+                        <Input {...field} type="email" />
+                        <ErrorMessage name="email" component="div" />
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Field name="password">
+                    {({
+                      field,
+                      form,
+                    }: {
+                      field: FieldInputProps<any>;
+                      form: FormikProps<any>;
+                    }) => (
+                      <FormControl id="password">
+                        <FormLabel>Password</FormLabel>
+                        <Input {...field} type="password" />
+                        <ErrorMessage name="password" component="div" />
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Stack spacing={10}>
+                    <Stack
+                      direction={{ base: "column", sm: "row" }}
+                      align={"start"}
+                      justify={"space-between"}
+                    >
+                      <Checkbox>Remember me</Checkbox>
+                      <Text color={"blue.400"}>Forgot password?</Text>
+                    </Stack>
+                    <Button
+                      type="submit"
+                      bg={"blue.400"}
+                      color={"white"}
+                      _hover={{
+                        bg: "blue.500",
+                      }}
+                      disabled={isSubmitting}
+                    >
+                      Sign in
+                    </Button>
+                  </Stack>
+                </Stack>
+              </Form>
+            </Box>
           </Stack>
-        </Box>
-      </Stack>
-    </Flex>
-  )
+        </Flex>
+      )}
+    </Formik>
+  );
 }
