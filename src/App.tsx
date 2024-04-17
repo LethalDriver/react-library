274d6862b639340545db1b075da0bd5871d17cf6
mwdziak <mwdziak@gmail.com>
2024-04-17 18:27:52 +0200

    Defined hooks is books component and rendered the books component in main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import Header from './components/Header'
-import Login from './components/Login'
-import './App.css'
+import Header from "./components/Header";
+import Login from "./components/Login";
+import "./App.css";
+import Books from "./components/Books";
 
 function App() {
-
   return (
     <div>
-      <Header/>
-      <Login/>
+      <Header />
+      <Login />
+      <Books />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,27 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+import { api } from "../service/httpService";
+import { Input, Stack } from "@chakra-ui/react";
 
 export type Book = {
-    id: number;
-    title: string;
-    author: string;
-    isbn: string;
-    publisher: string;
-    avaiableCopies: number;
-    bookDetails: BookDetails;
-}
+  id: number;
+  title: string;
+  author: string;
+  isbn: string;
+  publisher: string;
+  avaiableCopies: number;
+  bookDetails: BookDetails;
+};
 
 export type BookDetails = {
-    genre: string;
-    summary: string;
-    coverImage: string;
-}
+  genre: string;
+  summary: string;
+  coverImage: string;
+};
 
 const Books = () => {
-    const [books, setBooks] = useState<Book[]>([]);
+  const [books, setBooks] = useState<Book[]>([]);
+  const [search, setSearch] = useState<string>("");
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const fetchedBooks = await api.fetchBooks();
+      setBooks(fetchedBooks);
+    };
+    fetchBooks();
+  }, [search]);
+  const onSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+    console.log(search);
+  };
 
   return (
-    <div>Books</div>
-  )
-}
+    <Stack spacing={4}>
+      <Input
+        value={search}
+        onChange={onSearchChange}
+        placeholder="Search books"
+      />
+    </Stack>
+  );
+};
 
-export default Books
+export default Books;
