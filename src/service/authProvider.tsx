955970789beeb0714db4d624b47bc8f03b843681
mwdziak <mwdziak@gmail.com>
2024-04-18 19:04:55 +0200

    Formatted auth provider component

--- src/service/authProvider.tsx
+++ src/service/authProvider.tsx
@@ -2,32 +2,30 @@ import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import { userDto, api } from "./httpService";
 
 type AuthContextType = {
-    user: userDto | null;
-    setUser: (newUser: userDto) => void;
-    };
-
+  user: userDto | null;
+  setUser: (newUser: userDto) => void;
+};
 
 const AuthContext = createContext<AuthContextType>({
-    user: null,
-    setUser: (newUser: userDto) => {},
+  user: null,
+  setUser: (newUser: userDto) => {},
 });
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser_] = useState<userDto | null>(null);
 
-const [user, setUser_] = useState<userDto | null>(null);
-
-const setUser = (newUser: userDto) => {
-  setUser_(newUser);
-}
-
-useEffect(() => {
-  const fetchUser = async () => {
-    const userInfo = await api.userInfo();
-    setUser_(userInfo);
+  const setUser = (newUser: userDto) => {
+    setUser_(newUser);
   };
 
-  fetchUser();
-}, []);
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userInfo = await api.userInfo();
+      setUser_(userInfo);
+    };
+
+    fetchUser();
+  }, []);
 
   const contextValue = useMemo(
     () => ({
